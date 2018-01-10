89aaf0c8d132411a4e184f20eefa17fedd03a23e
dhazlettjr <d.hazlett.jr@gmail.com>
2018-01-10 15:14:00 -0600

    created ajax call with promise. installed moment.js for time

--- javascripts/factory.js
+++ javascripts/factory.js
@@ -2,3 +2,30 @@
 
 const $ = require('jquery');
 require('handlebars');
+
+module.exports.getData = () => {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "https://themepark-5f773.firebaseio.com/themePark/-L2W7UdQhglpEtROZ12K/attractions.json"
+      })
+        .done(data => {
+          resolve(data);
+          console.log(data);
+          
+        })
+        .fail(error => {
+          console.log("uh-oh", error.statusText);
+          reject(error);
+        });
+    });
+  };
+
+//   module.exports.timeFilter = () => {
+//       return new Promise((resolve, reject) => {
+//           $.ajax({
+//               url:
+//           })
+//       })
+//   }
+  
+  
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,13 @@
 "use strict";
 
 const $ = require('jquery');
+var moment = require('moment');
 require('handlebars');
+let factory = require('./factory');
+let timeNow = moment().format("HH");
+
+factory.getData()
+    .then((data) => {
+    });
+
+    console.log('time',timeNow);
