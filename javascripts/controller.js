5b02bacfe26cbee67f6d07ebcbf6f5b587eccd02
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-01-16 16:16:32 -0600

    set up function on controller to combine attraction data with types, trying to get time function to accept the data

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -21,3 +21,25 @@ factory.getInfo()
 .then((info) => {
     //console.log('info working', info);
 });
+
+module.exports.getType = (attrData) => {
+    return new Promise((resolve, reject) => {
+
+    let p1 = factory.getAttractionData();
+    let p2 = factory.getAttTypes();
+    let newDataWithTypes = [];
+        Promise.all([p1,p2])
+        .then((attrData) => { 
+            attrData[0].forEach(allAttractions => {
+                attrData[1].forEach(typeofAttractions => {
+                        if (allAttractions.type_id === typeofAttractions.id) {
+                            allAttractions.type = typeofAttractions.name;
+                            newDataWithTypes.push(allAttractions); 
+                        }
+                    });
+                });
+            }
+        );
+        resolve(newDataWithTypes);
+    });
+    };
